683a84ba64509de9d67a651cfdadab67793b82ba
Kyle Rockman <krockman@underarmour.com>
2015-12-28 09:15:41 -0600

    add default utterances for default intents

--- django_alexa/alexa.py
+++ django_alexa/alexa.py
@@ -4,7 +4,17 @@ from .api import intent, ResponseBuilder
 
 @intent
 def LaunchRequest():
-    """Default Start Session Intent"""
+    """
+    Default Start Session Intent
+    ---
+    launch
+    open
+    resume
+    start
+    run
+    load
+    begin
+    """
     return ResponseBuilder.create_response(message="Welcome. Say help if you would like help. What would you like to do next?",
                                            reprompt="What would you like to do next?",
                                            end_session=False)
@@ -12,7 +22,11 @@ def LaunchRequest():
 
 @intent
 def CancelIntent():
-    """Default Cancel Intent"""
+    """
+    Default Cancel Intent
+    ---
+    cancel
+    """
     return ResponseBuilder.create_response(message="Actions Canceled! What would you like to do next?",
                                            reprompt="What would you like to do next?",
                                            end_session=False)
@@ -20,17 +34,33 @@ def CancelIntent():
 
 @intent
 def StopIntent():
-    """Default Stop Intent"""
+    """
+    Default Stop Intent
+    ---
+    stop
+    end
+    """
     return ResponseBuilder.create_response(message="Stopping Actions. Goodbye!")
 
 
 @intent
 def HelpIntent():
-    """Default Help Intent"""
+    """
+    Default Help Intent
+    ---
+    help
+    info
+    information
+    """
     return ResponseBuilder.create_response(message="No help was configured!")
 
 
 @intent
 def SessionEndedRequest():
-    """Default End Session Intent"""
-    return ResponseBuilder.create_response(message="Have a nice day. Goodbye!")
+    """
+    Default End Session Intent
+    ---
+    quit
+    nevermind
+    """
+    return ResponseBuilder.create_response()

0a90d6167a2e97bc07615b3714e2621777bd4498
David Buxton <david@dbuxton.com>
2016-03-26 18:57:25 +0000

    Forgot `open_file`...

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from os import path
+
+def open_file(fname):
+    return open(path.join(path.dirname(__file__), fname))
 
 setup(
     setup_requires=['pbr', 'pyversion'],

d33888792f673b6b45c3cd8ecf8caf3de95abc3d
David Buxton <david@dbuxton.com>
2016-03-28 12:15:52 +0100

    Correct session

--- django_alexa/internal/intents_schema.py
+++ django_alexa/internal/intents_schema.py
@@ -31,7 +31,7 @@ class IntentsSchema():
         return cls.intents[key_name]
 
     @classmethod
-    def route(cls, session, app, intent, intent_kwargs, validated_data=None):
+    def route(cls, session, app, intent, intent_kwargs):
         if validated_data is None:
             validated_data = {}
         """Routes an intent to the proper method"""
@@ -39,8 +39,7 @@ class IntentsSchema():
         if slot and bool(intent_kwargs) is False:
             msg = "Intent '{0}.{1}' requires slots data and none was provided".format(app, intent)
             raise InternalError(msg)
-        intent_kwargs['session'] = session.get('attributes', {})
-        intent_kwargs['validated_data'] = validated_data
+        intent_kwargs['session'] = session
         msg = "Routing: '{0}.{1}' with args {2} to '{3}.{4}'".format(app,
                                                                      intent,
                                                                      intent_kwargs,
--- django_alexa/views.py
+++ django_alexa/views.py
@@ -50,7 +50,7 @@ class ASKView(APIView):
             slots = slot(data=intent_kwargs)
             slots.is_valid()
             intent_kwargs = slots.data
-        data = IntentsSchema.route(session, app, intent_name, intent_kwargs, validated_data)
+        data = IntentsSchema.route(session, app, intent_name, intent_kwargs)
         return Response(data=data, status=HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):

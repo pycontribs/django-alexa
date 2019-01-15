99dcb73c5e55b8ac25ac5447225d8242b8194ae0
Ling Li <ling.li@utiliteam.co.uk>
2019-01-15 07:55:26 +0000

    use sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,13 +20,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'django-alexa documentation'
-copyright = '2019, Kyle Rockman / Ling Li'
-author = 'Kyle Rockman / Ling Li'
+copyright = '2019, Kyle Rockman'
+author = 'Kyle Rockman'
 
 # The short X.Y version
-version = ''
+version = '0.0.7'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
@@ -79,7 +79,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
